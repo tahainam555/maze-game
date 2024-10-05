e07036da3cd0893b3b28ed5562fb91511189bc40
tahainam555 <tahainam879@gmail.com>
2024-10-05 18:19:52 +0500

    random placement of player, coins etc.

--- q1.cpp
+++ q1.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<curses.h>
+#include<ctime>
 using namespace std;
 
 class Node{
@@ -22,8 +23,8 @@ class Board{
         }
 
         void setBoard(int s){
+            srand(time(0));
             size=s;
-            char chh='a';
             Node* my_board[size][size];
             for(int i=0 ; i<size; i++){
                 for(int j=0 ; j<size; j++){
@@ -32,11 +33,10 @@ class Board{
                     my_board[i][j]->down=NULL;
                     my_board[i][j]->right=NULL;
                     my_board[i][j]->left=NULL;
-                    my_board[i][j]->ch=chh++;
+                    my_board[i][j]->ch='-';
                 }
             }
-            head = my_board[0][0];
-
+            head = my_board[0][0];       
             for(int i=0 ; i<size; i++){
                 for(int j=0 ; j<size; j++){
                     if(i>0){
@@ -53,29 +53,64 @@ class Board{
                     }
                 }
             }
-        
+            for(int i=0 ; i<3; i++){
+                my_board[rand()%size][rand()%size]->ch='c';
+                my_board[rand()%size][rand()%size]->ch='B';    
+            }
+            int rand1, rand2;
+            bool check=true;
+            rand1=rand()%size;
+            rand2=rand()%size;
+            my_board[rand1][rand2]->ch='k';
+            rand1=rand()%size;
+            rand2=rand()%size;
+            while(check){
+                if(my_board[rand1][rand2]->ch=='-'){
+                    my_board[rand1][rand2]->ch='d';
+                    check=false;
+                }
+            }
+            rand1=rand()%size;
+            rand2=rand()%size;
+            check=true;
+            while(check){
+                if(my_board[rand1][rand2]->ch=='-'){
+                    my_board[rand1][rand2]->ch='P';
+                    check=false;
+                }
+            }
         }
 
         void display(){
             Node* curr = head;
+            for(int i=0 ; i<=size+1; i++){
+                cout << "# ";
+            }
+            cout << endl;
             for(int i=0 ; i<size; i++){
+                cout<<"# ";
                 curr=head;
                 for(int j=0 ; j<i; j++){
                     curr=curr->down;
                 }
                 for(int j=0 ; j<size; j++){
-                    cout << curr->ch;
+                    cout << curr->ch << " ";
                     curr = curr->right;
                 }
+                cout << "# ";
                 cout << endl;
             }
+            for(int i=0 ; i<=size+1; i++){
+                cout << "# ";
+            }
+            cout << endl;
         }
 
 };
 
 int main(){
     Board B1;
-    B1.setBoard(3);
+    B1.setBoard(9);
     B1.display();
     
 }
