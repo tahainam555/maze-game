c413fbb5244234e719379be6bde1cc8e6ca84183
tahainam555 <tahainam879@gmail.com>
2024-10-07 21:29:35 +0500

    fixing pointers of nodes

--- q1.cpp
+++ q1.cpp
@@ -87,50 +87,92 @@ class Board{
             }
         }
 
+        void my_board(){
+            Node* curr = NULL;
+            Node* curr1 = NULL;
+            Node* curr2 = NULL;
+            Node* prev = NULL;
+            for(int i=0; i<size; i++){
+                curr = NULL;
+                prev = curr2;
+                for(int j=0; j<size; j++){
+                    Node* temp = new Node();
+                    temp->right=NULL;
+                    temp->left=NULL;
+                    temp->down=NULL;
+                    temp->up=NULL;
+                    temp->ch='-';
+                    if(head==NULL){
+                        head = temp;
+                        curr1 = head;
+                    }
+                    if(curr==NULL){
+                        curr1 = temp;
+                        if(prev!=NULL){
+                            prev->down=temp;
+                            temp->up=prev;
+                            prev=prev->right;
+                        }
+                    } 
+                    else{
+                        curr->right=temp;
+                        temp->left=curr;
+                        if(prev!=NULL){
+                            prev->down = temp;
+                            temp->up = prev;
+                            prev = prev->right;
+                        }
+                    }
+                    curr=temp;
+                }
+                curr2=curr1;
+            }
+        }
+
         void setBoard(){
             srand(time(0));
-            Node* my_board[size][size];
-            for(int i=0 ; i<size; i++){
-                for(int j=0 ; j<size; j++){
-                    my_board[i][j]=new Node();
-                    my_board[i][j]->up=NULL;
-                    my_board[i][j]->down=NULL;
-                    my_board[i][j]->right=NULL;
-                    my_board[i][j]->left=NULL;
-                    my_board[i][j]->ch='-';
+            my_board();
+            Node* curr = head;
+            for(int i=0 ; i<6; i++){
+                curr=head;
+                int num1=rand()%size;
+                int num2=rand()%size;
+                for(int j=0 ; j<num1 ; j++){
+                    curr=curr->right;
                 }
-            }
-            head = my_board[0][0];       
-            for(int i=0 ; i<size; i++){
-                for(int j=0 ; j<size; j++){
-                    if(i>0){
-                        my_board[i][j]->up=my_board[i-1][j];
-                    }
-                    if(j>0){
-                        my_board[i][j]->left=my_board[i][j-1];
-                    }
-                    if(i<size-1){
-                        my_board[i][j]->down=my_board[i+1][j];
-                    }
-                    if(j<size-1){
-                        my_board[i][j]->right=my_board[i][j+1];
-                    }
+                for(int j=0 ; j<num2 ; j++){
+                    curr=curr->down;
                 }
+                if(i%2==0)
+                    curr->ch='B';
+                else
+                    curr->ch='c';
             }
-            for(int i=0 ; i<3; i++){
-                my_board[rand()%size][rand()%size]->ch='B';    
-                my_board[rand()%size][rand()%size]->ch='c';
-            }
+    
             int rand1, rand2;
             bool check=true;
             rand1=rand()%size;
             rand2=rand()%size;
-            my_board[rand1][rand2]->ch='k';
+            curr=head;
+            for(int j=0 ; j<rand1 ; j++){
+                curr=curr->right;
+            }
+            for(int j=0 ; j<rand2 ; j++){
+                curr=curr->down;
+            }
+            curr->ch='k';
             rand1=rand()%size;
             rand2=rand()%size;
             while(check){
-                if(my_board[rand1][rand2]->ch=='-'){
-                    my_board[rand1][rand2]->ch='d';
+                curr=head;
+                for(int j=0 ; j<rand1 ; j++){
+                    curr=curr->right;
+                }
+                for(int j=0 ; j<rand2 ; j++){
+                    curr=curr->down;
+                }
+                if(curr->ch=='-'){
+                    curr->ch='d';
                     check=false;
                 }
                 else{
@@ -142,8 +184,15 @@ class Board{
             rand2=rand()%size;
             check=true;
             while(check){
-                if(my_board[rand1][rand2]->ch=='-'){
-                    my_board[rand1][rand2]->ch='P';
+                curr=head;
+                for(int j=0 ; j<rand1 ; j++){
+                    curr=curr->right;
+                }
+                for(int j=0 ; j<rand2 ; j++){
+                    curr=curr->down;
+                }
+                if(curr->ch=='-'){
+                    curr->ch='P';
                     check=false;
                 }
                 else{
